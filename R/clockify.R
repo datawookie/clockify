3756a67d56b61a47edcc159899e24e81a00cee07
Vusani <vusanimasocha@gmail.com>
2021-05-26 07:51:43 +0000

    Clockify documentation.

--- R/clockify.R
+++ R/clockify.R
@@ -1,28 +1,22 @@
-#' @import httr
-#' @import dplyr
-#' @import tidyr
-#' @import purrr
-#' @import janitor
-#' @import logger
-#' @import anytime
-NULL
-
-#' Title
+#' Clockify
 #'
-#' @param api_key
+#' @param api_key A key to connect to clockify generated from the clockify settings.
 #'
 #' @return
 #' @export
 #'
-#' @examples
+#' @examples set_api_key(api_key = "MGRiNGIyNGUtMzRmYS00ODg5LThkNmEtNWVjMWY1YWIxYzc2")
 set_api_key <- function(api_key) {
   log_debug("Setting API key: {api_key}.")
   assign("api_key", api_key, envir = cache)
 }
 
-#' Title
+#' Get API key
+#' Searches for the API key in the cache then defines "billable", "description", "error",
+#' "project_id", "timeInterval", "time_end", "time_start" and  "workspace_id" globally.
 #'
-#' @return
+#' @return Returns "billable", "description", "error", "project_id", "timeInterval", "time_end",
+#'  "time_start" and  "workspace_id".
 #' @export
 #'
 #' @examples
