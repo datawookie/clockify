8dbfb18c0a7f1fc0ab2889848bed67941f95ac0a
Andrew Collier <andrew@fathomdata.dev>
2021-09-03 03:21:07 +0100

    fix: Skip tests if no API key in environment

--- tests/testthat/test-api-key.R
+++ tests/testthat/test-api-key.R
@@ -4,11 +4,15 @@ test_that("error on empty api key", {
 
 test_that("set api key", {
   skip_on_cran()
+  skip_if(NO_API_KEY_IN_ENVIRONMENT)
+
   set_api_key(CLOCKIFY_API_KEY)
   expect_equal(cache_get(API_KEY), CLOCKIFY_API_KEY)
 })
 
 test_that("get api key", {
   skip_on_cran()
+  skip_if(NO_API_KEY_IN_ENVIRONMENT)
+
   expect_equal(get_api_key(), CLOCKIFY_API_KEY)
 })
--- tests/testthat/test-user.R
+++ tests/testthat/test-user.R
@@ -3,18 +3,24 @@ USERS_COLS <- c("user_id", "email", "user_name", "active_workspace", "default_wo
 
 test_that("get active users", {
   skip_on_cran()
+  skip_if(NO_API_KEY_IN_ENVIRONMENT)
+
   users <- users(active = TRUE)
   expect_identical(names(users), USERS_COLS_CONCISE)
 })
 
 test_that("get all users", {
   skip_on_cran()
+  skip_if(NO_API_KEY_IN_ENVIRONMENT)
+
   users <- users(active = FALSE)
   expect_identical(names(users), USERS_COLS_CONCISE)
 })
 
 test_that("extended output", {
   skip_on_cran()
+  skip_if(NO_API_KEY_IN_ENVIRONMENT)
+
   users <- users(concise = FALSE)
   expect_identical(names(users), USERS_COLS)
 })
