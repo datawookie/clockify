b08a09c21024b7594fa47ddf591289032934d8b1
Andrew Collier <andrew@fathomdata.dev>
2022-08-28 14:31:41 +0100

    fix: Cleaner implementation of list_remove_empty()

--- R/util.R
+++ R/util.R
@@ -11,7 +11,5 @@ list_null_to_na <- function(l) {
 }
 
 list_remove_empty <- function(l) {
-  l <- l[!sapply(l, is.null)]
-  l <- l[sapply(l, length) != 0]
-  l
+  rlist::list.clean(l, recursive = TRUE)
 }
